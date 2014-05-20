84e33b5c397af25a727c73b00eedb3d6c92cab6e
rossmacgregor <rossmacgregor@2a36de45-7175-4c33-961a-326e2a3a194b>
2014-05-20 19:25:21 +0000

    Repaired Windows build.

--- source/AsioExpress/ClientServer/RoundRobinServer.cpp
+++ source/AsioExpress/ClientServer/RoundRobinServer.cpp
@@ -3,6 +3,8 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include "AsioExpress/pch.hpp"
+
 #include "AsioExpress/ClientServer/RoundRobinServer.hpp"
 
 namespace AsioExpress {
